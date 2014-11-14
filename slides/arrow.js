575c2b0efc78347730f101089aa94026e5aba43f
Trace Pillars <trace.pillars@e7solutions.com>
2014-11-14 11:36:13 -0500

    should be done for now

---
+++ slides/arrow.js
@@ -0,0 +1,18 @@
+var a = ["Hydrogen", "Helium", "Lithium", "Beryllium"];
+var b = a.map(function(s){ return s.length });
+var c = a.map( s => s.length );
+console.log(b);
+console.log(c);
+
+var f = new Foo();
+function Foo(){
+    this.color = "red";
+
+    setTimeout(function(){
+        if(typeof this.color !== "undefined") {
+            $("body").attr("style", "background-color: " + this.color);
+        }
+    }, 300);
+
+    setTimeout(x => $("arrow_textarea").attr("style", "background-color: " + this.color), 300);
+}
