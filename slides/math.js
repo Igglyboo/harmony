804dd290bacdc31991023a4bc2374ae4639e84c1
Trace Pillars <trace.pillars@e7solutions.com>
2014-11-14 10:32:00 -0500

    slide 4

---
+++ slides/math.js
@@ -0,0 +1,8 @@
+console.log(Math.cosh(0));
+console.log(Math.tanh(1));
+console.log(Math.trunc(3.5678));
+
+console.log(Number.isFinite(1));
+console.log(Number.isFinite(Infinity));
+console.log(Number.isNaN(NaN));
+console.log(Number.isInteger(1.34));
